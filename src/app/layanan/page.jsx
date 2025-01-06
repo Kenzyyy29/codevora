9767cf2c131f99cfb8177e9c7c4aa8dc263be63a
Kenzyyy29 <kenzyworld29@gmail.com>
2025-01-07 00:45:59 +0700

    update:frontend

--- src/app/layanan/page.jsx
+++ src/app/layanan/page.jsx
@@ -86,6 +86,46 @@ export default function Layanan() {
     </div>
    </Link>
 
+   {/*Layanan Lain */}
+   <section className="bg-primary py-[8%] lg:py-[4%] 2xl:py-[3%] h-auto hidden md:block lg:block xl:block 2xl:block">
+    <div className="text-tulang text-center font-bold lg:text-2xl">
+     {" "}
+     LAYANAN LAIN
+    </div>
+    <div className="flex text-center justify-center gap-48 py-10">
+     <div
+      id="layanan-lain"
+      className="bg-tulang text-primary h-96 w-96 rounded-3xl text-2xl font-bold py-5">
+      DESAIN UI/UX
+      <p className="font-normal text-lg py-5 text-justify text-balance px-5">
+       Pengen bikin website buat usaha, tapi bingung harus mulai dari mana?
+       Tenang, di <span className="font-bold">CODEVORA</span> kalian bisa
+       menggunakan jasa kami untuk mendesain UI/UX impian kalian loh, kalian
+       juga bisa request desain yang kalian inginkan yang tentunya dengan harga
+       yang sangat terjangkau.
+      </p>
+      <button className="border rounded-full bg-primary text-tulang py-2 px-6 font-normal text-lg mt-3 transition-all ease-in-out duration-500 hover:bg-tulang hover:text-primary hover:border-primary">
+       Pesan UI/UX Sekarang
+      </button>
+     </div>
+     <div
+      id="layanan-lain"
+      className="bg-tulang text-primary h-96 w-96 rounded-3xl text-2xl font-bold py-5">
+      KONSULTASI
+      <p className="font-normal text-lg py-5 text-justify text-balance px-5">
+       Kalian masih bingung waktu udah punya usaha tapi gatau apa yang harus
+       dilakuin biar iklan digital kalian naik? Tenang, di{" "}
+       <span className="font-bold">CODEVORA</span> Kalian bisa konsultasi
+       terkait iklan digital kalian loh! Kita bakal bantu iklan digital usaha
+       kalian supaya orang orang bisa ngejangkau akun kalian.
+      </p>
+      <button className="border rounded-full bg-primary text-tulang py-2 px-6 font-normal text-lg mt-3 transition-all ease-in-out duration-500 hover:bg-tulang hover:text-primary hover:border-primary">
+       Konsultasi Sekarang
+      </button>
+     </div>
+    </div>
+   </section>
+
    {/*Mobile Layanan Lain */}
    <section className="h-auto bg-primary block md:hidden lg:hidden xl:hidden 2xl:hidden text-center pb-10">
     <h1 className="text-center text-tulang font-bold text-2xl py-10 ml-3 xl:ml-10">
@@ -109,7 +149,7 @@ export default function Layanan() {
      <SwiperSlide>
       <div
        id="layanan-lain"
-       className="bg-tulang text-primary h-96 pb- mx-5 rounded-3xl text-2xl font-semibold py-5">
+       className="bg-tulang text-primary h-96 mx-5 rounded-3xl text-2xl font-bold py-5">
        DESAIN UI/UX
        <p className="font-normal text-lg py-5 text-justify text-balance px-5">
         Pengen bikin website buat usaha, tapi bingung harus mulai dari mana?
@@ -124,11 +164,14 @@ export default function Layanan() {
       </div>
      </SwiperSlide>
      <SwiperSlide>
-      <div
-       className="bg-tulang text-primary h-96 pb- mx-5 rounded-3xl text-2xl font-semibold py-5">
+      <div className="bg-tulang text-primary h-96 mx-5 rounded-3xl text-2xl font-bold py-5">
        KONSULTASI
        <p className="font-normal text-lg py-5 text-justify text-balance px-5">
-        Kalian masih bingung waktu udah punya usaha tapi gatau apa yang harus dilakuin biar iklan digital kalian naik? Tenang, di <span className="font-bold">CODEVORA</span> Kalian bisa konsultasi terkait iklan digital kalian loh! Kita bakal bantu iklan digital usaha kalian supaya orang orang bisa ngejangkau akun kalian. 
+        Kalian masih bingung waktu udah punya usaha tapi gatau apa yang harus
+        dilakuin biar iklan digital kalian naik? Tenang, di{" "}
+        <span className="font-bold">CODEVORA</span> Kalian bisa konsultasi
+        terkait iklan digital kalian loh! Kita bakal bantu iklan digital usaha
+        kalian supaya orang orang bisa ngejangkau akun kalian.
        </p>
        <button className="border rounded-full bg-primary text-tulang py-2 px-6 font-normal text-lg mt-3 transition-all ease-in-out duration-500 hover:bg-tulang hover:text-primary hover:border-primary">
         Konsultasi Sekarang
