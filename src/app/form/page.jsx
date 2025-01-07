e2763df016f48a5487d84cee0f30bf334f8b6b78
Kenzyyy29 <kenzyworld29@gmail.com>
2025-01-07 17:31:42 +0700

    update:form

--- src/app/form/page.jsx
+++ src/app/form/page.jsx
@@ -1,9 +1,55 @@
 import {PhotoIcon, UserCircleIcon} from "@heroicons/react/24/solid";
 import {ChevronDownIcon} from "@heroicons/react/16/solid";
+import Link from "next/link";
 
 export default function Form() {
  return (
-  <section className="h-screen bg-primary lg:py-[20%] 2xl:py-[10%] py-[80%]">
+  <div className=" bg-primary text-center justify-center lg:py-[20%] 2xl:py-[20%] py-[80%] h-screen">
+   <div className="text-hitam border rounded-lg bg-tulang w-auto max-w-[31.6%] text-start font-semibold text-base px-5 ml-10">
+    <label
+     className="block uppercase tracking-wide text-gray-700 text-base font-bold mb-2"
+     htmlFor="grid-first-name">
+     First Name
+    </label>
+    <input
+     className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+     id="grid-first-name"
+     type="text"
+     placeholder="Masukkan nama anda"
+    />
+    <label
+     className="block uppercase tracking-wide text-gray-700 text-base font-bold mb-2"
+     htmlFor="grid-first-name">
+     E-mail
+    </label>
+    <input
+     className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+     id="grid-first-name"
+     type="email"
+     placeholder="Mohon masukkan alamat email anda"
+    />
+    <label
+     className="block uppercase tracking-wide text-gray-700 text-base font-bold mb-2"
+     htmlFor="grid-first-name">
+     Pengaduan
+    </label>
+    <input
+     className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+     id="grid-first-name"
+     type="email"
+     placeholder="Mohon masukkan alamat email anda"
+    />
+   </div>
+   <Link href="/">
+    <button className="border rounded-full bg-primary text-tulang py-2 px-6 font-semibold mt-10 transition-all ease-in-out duration-500 hover:bg-tulang hover:text-primary hover:border-primary">
+     Kembali
+    </button>
+   </Link>
+  </div>
+ );
+}
+
+{/*<section className="h-screen bg-primary lg:py-[20%] xl:py-[10%] 2xl:py-[10%] py-[80%]">
    <form className="w-screen max-w-lg border border-secondary rounded-3xl ml-40 py-5 px-5 bg-tulang">
     <div className="flex flex-wrap -mx-3 mb-6">
      <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
@@ -19,19 +65,19 @@ export default function Form() {
        placeholder="Jane"
       />
      </div>
-     <div className="w-full md:w-1/2 px-3">
-      <label
-       className="block uppercase tracking-wide text-gray-700 text-base font-bold mb-2"
-       htmlFor="grid-last-name">
-       Last Name
-      </label>
-      <input
-       className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-       id="grid-last-name"
-       type="text"
-       placeholder="Doe"
-      />
-     </div>
+    </div>
+    <div className="w-full md:w-1/2 px-3">
+     <label
+      className="block uppercase tracking-wide text-gray-700 text-base font-bold mb-2"
+      htmlFor="grid-last-name">
+      Alamat E-mail
+     </label>
+     <input
+      className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+      id="grid-last-name"
+      type="text"
+      placeholder="Doe"
+     />
     </div>
     <div className="flex flex-wrap -mx-3 mb-6">
      <div className="w-full px-3">
@@ -108,5 +154,4 @@ export default function Form() {
     </div>
    </form>
   </section>
- );
-}
+ );*/}
