6a16ccf5a701e8f1a9a491c05f1e65d9e8ea3a80
Codevora <codevoratech@gmail.com>
2025-01-10 21:08:10 +0700

    fix:form:2xl

--- src/components/Posts/CardList.jsx
+++ src/components/Posts/CardList.jsx
@@ -1,9 +1,9 @@
 const CardList = ({children} ) => {
  return (
   <div
-   className="h-auto lg:h-auto xl:h-auto 2xl:h-auto bg-cover bg-center 2xl:py-[7.5%] xl:pb-[3.5%] 2xl:px-40 xl:px-32 px-4 pb-14"
+   className="h-auto lg:h-auto xl:h-auto 2xl:h-screen bg-cover bg-center 2xl:py-[2%] xl:pb-[3.5%] 2xl:px-40 xl:px-32 px-4 pb-14"
    style={{backgroundImage: "url('/bg3.jpg')"}}>
-   <div className="text-center text-tulang font-bold text-2xl py-10 2xl:py-10 xl:py-10">
+   <div className="text-center text-tulang font-bold text-2xl py-10 2xl:pb-24 xl:py-10">
     LAYANAN PENGADUAN
    </div>
    {children}
